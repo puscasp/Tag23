66cb91f5c58262d7b91b4ca1253975b2250bd48e
puscasp <paul.puscas01@gmail.com>
2023-01-11 18:54:30 -0500

    vert

--- src/tag.cpp
+++ src/tag.cpp
@@ -21,13 +21,26 @@ int main() {
 		return -1;
 	}
 
-	
+
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
 
 
 
 	glViewport(0, 0, 800, 600);
+	float vertices[] =
+	{
+		-0.5,-0.5,0.0,
+		0.5,-0.5,0.0,
+		0.0,0.5,0.0
+
+	};
+	unsigned int VBO;
+	glGenBuffers(1, &VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
 
 
 	while (!glfwWindowShouldClose(window)) {
