236a87a34c38871861d64932151dccb5c68cc200
puscasp <paul.puscas01@gmail.com>
2023-01-12 14:53:47 -0500

    shader source + no more executables

--- src/tag.cpp
+++ src/tag.cpp
@@ -41,6 +41,41 @@ int main() {
 
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
+	//Vertex shader code
+	const char* vertexShaderSource = "#version 330 core\n"
+		"layout (location = 0) in vec3 aPos;\n"
+		"void main()\n"
+		"{\n"
+		" gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+		"}\0";
+	unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	//Note that &vertexShaderSource can be used to open a file, good for migrating
+	//in final copy https://stackoverflow.com/questions/22100408/what-is-the-meaning-of-the-parameters-to-glshadersource
+
+	glCompileShader(vertexShader);
+
+
+
+	//Fragment shader code
+	const char* fragmentShaderSource = "#version 330 core\n"
+		"out vec4 FragColor;\n"
+		"void main()\n"
+		"{\n"
+		"FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+		"}\n";
+	unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	glCompileShader(fragmentShader);
+
+
+	//Shader program object
+
+	unsigned int shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+
 
 
 	while (!glfwWindowShouldClose(window)) {
