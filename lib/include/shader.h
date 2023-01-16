6156e93190443e72a0847f91ad951affa98d8449
puscasp <paul.puscas01@gmail.com>
2023-01-16 15:19:32 -0500

    added shader header

---
+++ lib/include/shader.h
@@ -0,0 +1,20 @@
+class shader {
+
+	//retrieve shader source code
+		//open file
+		//read file into stream
+		//close file
+		//convert string to stream
+	
+
+	//compile shaders
+		//throw compile errors
+
+
+	//link shaders
+		//throw link errors
+
+	//delete shaders
+
+
+};
