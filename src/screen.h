51c84b4b75ca05ca589cf4dc77fbafb8906d7504
puscasp <paul.puscas01@gmail.com>
2023-07-26 20:35:30 -0400

    Implemented Screen buffer creation

--- src/screen.h
+++ src/screen.h
@@ -6,42 +6,16 @@ Visual display of buttons and graphical elements. Is kept as a group. Has a vert
 #include <types.h>
 #include <vector>
 #include <uielement.h>
+#include <unordered_map>
+class screen{
 
+	unsigned int screenEBO, screenVBO;
+	std::vector<Index> screenIndexList;
+	std::vector<Vertex> screenVertexList;
 
-unsigned int screenEBO, screenVBO;
+	void returnEBO(GLuint* EBOptr, GLuint* VBOptr); 
 
-
-void returnEBO(GLuint *EBOptr, GLuint *VBOptr) {
-
-	std::vector<Vertex> vertexList;
-	std::vector<unsigned int> indexList;
-
-
-
-
-	/*
-	foreach uielement
-		foreach vertex
-		
-			add to hashmap
-			if unique: add to vertexlist
-			add to indexlist
-	
-	
-	*/
-
-
-
-
-	glGenVertexArrays(1, EBOptr);
-	glGenBuffers(1, VBOptr);
-
-	glBindBuffer(GL_ARRAY_BUFFER, *VBOptr);
-	glBufferData(GL_ARRAY_BUFFER, vertexList.size() * sizeof(Vertex), &vertexList[0], GL_STATIC_DRAW);
-
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, *EBOptr);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, indexList.size() * sizeof(unsigned int), &indexList[0], GL_STATIC_DRAW);
-}
+};
 
 
 
--- src/uielement.h
+++ src/uielement.h
@@ -1,6 +1,15 @@
-
 #include <vector>;
-
+#include <types.h>;
 unsigned int *parentScreenVBO;
 std::vector<unsigned int>vertexCoords;
 
+class uiElement
+{
+	unsigned int* parentScreenVBO;
+	std::vector<Vertex>elementCoords;
+public:
+	std::vector<Vertex>getVerts()
+	{
+		return elementCoords;
+	}
+};
